949f6e11a9429ce1508dc1511a2a38f2ce6ec0bd
1exuss <natanalexandre8@gmail.com>
2021-09-17 21:54:50 -0300

    ajuste 2.0 pra aula

--- App.Api/Controllers/CidadeController.cs
+++ App.Api/Controllers/CidadeController.cs
@@ -1,5 +1,7 @@
-﻿using App.Domain.Entities;
+﻿using App.Domain.DTO;
+using App.Domain.Entities;
 using App.Domain.Interfaces.Application;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 
@@ -17,34 +19,80 @@ namespace App.Api.Controllers
         }
 
         [HttpGet("ListaCidades")]
+        [AllowAnonymous]
         public JsonResult ListaCidades()
         {
-            return Json(_service.listaCidades());
+            try
+            {
+                var obj =_service.listaCidades();
+                return Json(RetornoApi.Sucesso(obj));
+            }
+            catch(Exception ex)
+            {
+                return Json(RetornoApi.Erro(ex.Message));
+            }
+            
         }
 
         [HttpGet("BuscaPorId")]
+        [AllowAnonymous]
         public JsonResult BuscaPorId(Guid id)
         {
-            return Json(_service.BuscaPorId(id));
+            try
+            {
+                var obj = _service.BuscaPorId(id);
+                return Json(RetornoApi.Sucesso(obj));
+            }
+            catch (Exception ex)
+            {
+                return Json(RetornoApi.Erro(ex.Message));
+            }
+           // return Json(_service.BuscaPorId(id));
         }
         [HttpPost("Salvar")]
+        [AllowAnonymous]
         public JsonResult Salvar(string nome, string cep, string uf)
         {
-            var obj = new Cidade
+            try
             {
-                Nome = nome,
-                CEP = cep,
-                UF = uf
-            };
-            _service.Salvar(obj);
-            return Json(true);
+                var obj = new Cidade
+                {
+                    Nome = nome,
+                    CEP = cep,
+                    UF = uf
+                };
+                _service.Salvar(obj);
+                return Json(RetornoApi.Sucesso(obj));
+            }
+            catch (Exception ex)
+            {
+                return Json(RetornoApi.Erro(ex.Message));
+            }
+          //  var obj = new Cidade
+          //  {
+          //      Nome = nome,
+            //    CEP = cep,
+            //    UF = uf
+          //  };
+          //  _service.Salvar(obj);
+           // return Json(true);
         }
 
         [HttpDelete("Remover")]
+        [AllowAnonymous]
         public JsonResult Remover(Guid id)
         {
-            _service.Remover(id);
-            return Json(true);
+            try
+            {
+                _service.Remover(id);
+                return Json(RetornoApi.Sucesso(true));
+            }
+            catch (Exception ex)
+            {
+                return Json(RetornoApi.Erro(ex.Message));
+            }
+           // _service.Remover(id);
+          //  return Json(true);
         }
     }
 }
--- App.Api/Controllers/PessoaController.cs
+++ App.Api/Controllers/PessoaController.cs
@@ -1,5 +1,7 @@
-﻿using App.Domain.Entities;
+﻿using App.Domain.DTO;
+using App.Domain.Entities;
 using App.Domain.Interfaces.Application;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 
@@ -23,30 +25,70 @@ namespace App.Api.Controllers
         }
 
         [HttpGet("BuscaPorId")]
+        [AllowAnonymous]
         public JsonResult BuscaPorId(Guid id)
         {
-            return Json(_service.BuscaPorId(id));
+            try
+            {
+                var obj = _service.BuscaPorId(id);
+                return Json(RetornoApi.Sucesso(obj));
+            }
+            catch (Exception ex)
+            {
+                return Json(RetornoApi.Erro(ex.Message));
+            }
+            //  return Json(_service.BuscaPorId(id));
         }
         [HttpPost("Salvar")]
+        [AllowAnonymous]
         public JsonResult Salvar(string nome, int peso, DateTime dataNascimento, bool ativo, Guid idCidade)
         {
-            var obj = new Pessoa
+            try
             {
-                Nome = nome,
-                DataNascimento = dataNascimento,
-                Peso = peso,
-                Ativo = ativo,
-                CidadeId = idCidade
-            };
-            _service.Salvar(obj);
-            return Json(true);
+                var obj = new Pessoa
+                {
+                    Nome = nome,
+                    DataNascimento = dataNascimento,
+                    Peso = peso,
+                    Ativo = ativo,
+                    CidadeId = idCidade
+                };
+                _service.Salvar(obj);
+                return Json(RetornoApi.Sucesso(obj));
+            }
+            catch (Exception ex)
+            {
+                return Json(RetornoApi.Erro(ex.Message));
+            }
         }
+        //var obj = new Pessoa
+        // {
+        //      Nome = nome,
+        //    DataNascimento = dataNascimento,
+        //   Peso = peso,
+        //    Ativo = ativo,
+        //  CidadeId = idCidade
+        //  };
+        //   _service.Salvar(obj);
+        //  return Json(true);
+        //  }
 
         [HttpDelete("Remover")]
+        [AllowAnonymous]
         public JsonResult Remover(Guid id)
         {
-            _service.Remover(id);
-            return Json(true);
+            try
+            {
+                _service.Remover(id);
+                return Json(RetornoApi.Sucesso(true));
+            }
+            catch (Exception ex)
+            {
+                return Json(RetornoApi.Erro(ex.Message));
+            }
+
+            //_service.Remover(id);
+            //  return Json(true);
         }
     }
 }
